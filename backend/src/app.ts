64889e6592e5c6137738fe7b1a7527eb7c948ddf
Ronohayon27 <ronohayon27@gmail.com>
2025-07-12 21:30:19 +0300

    setup logger

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,16 +1,29 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import morgan from "morgan";
+import logger from "./utils/logger";
 
 dotenv.config();
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Use Morgan to log HTTP requests
+app.use(
+  morgan("dev", {
+    stream: {
+      write: (message) => logger.info(message.trim()), // Pipe to Pino
+    },
+  })
+);
+
 app.get("/", (_req, res) => {
-  res.send("E-commerce API running");
+  logger.info("Root route hit");
+  res.send("E-commerce backend running");
 });
 
 export default app;
