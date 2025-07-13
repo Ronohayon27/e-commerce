dd760f15354918fb45283c27cd4c9c2466885758
Ronohayon27 <ronohayon27@gmail.com>
2025-07-13 21:30:40 +0300

    setting up front

---
+++ frontend/storigin/src/app/(site)/layout.tsx
@@ -0,0 +1,15 @@
+import Header from "@/components/Header";
+import "../globals.css";
+
+export default function SiteLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="flex min-h-screen items-center flex-col pb-10">
+      <Header />
+      {children}
+    </div>
+  );
+}
